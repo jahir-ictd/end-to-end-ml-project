9b3d0b4322183192b50af84e5c908fe01e41b997
jahir-ictd <jahir.ictd@gmail.com>
2025-08-24 12:54:01 +0600

    initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Jahirul",
+    author_email="jahirul@gmail.com",
+    packages=find_packages()
+)
